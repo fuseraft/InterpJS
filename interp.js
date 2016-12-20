967c541b9345f511a421eabbfa9d8f6331504e8f
Scott Christopher Stauffer <scstauf@gmail.com>
2016-12-20 16:25:50 -0600

    Fixed index bug

--- interp.js
+++ interp.js
@@ -6,6 +6,7 @@
     String.prototype.interp = function (v) {
         var s = this.valueOf(),
             a = [],
+            ins = '',
             ns = '',
             b = false,
             e = false,
@@ -16,6 +17,7 @@
             c = '';
 
         if (!s || l === 0) return '';
+
         if (v.constructor === Array) a = v;
         else
             for (i = 0; i < al; i++)
@@ -24,38 +26,46 @@
         for (i = 0; i < l; i++) {
             c = s[i];
             if (' \t\n\r\v'.indexOf(c) > -1) {
-                ns += c;
+                ins += c;
                 continue;
             }
-            if (!isNaN(c) && b)
-                n = parseInt(c);
+            if (!isNaN(c) && b) {
+                //n = parseInt(c);
+                ns += c;
+            }
             else {
                 e = (s[i - 1] === '\\');
                 switch (c) {
                     case '\\':
                         if (e)
-                            ns += c;
+                            ins += c;
                         break;
                     case '{':
                         if (e)
-                            ns += c;
+                            ins += c;
                         else
                             b = true;
                         break;
                     case '}':
                         if (e)
-                            ns += c;
+                            ins += c;
                         else if (b) {
-                            ns += a[n];
+                            if (ns.length > 0) {
+                                n = parseInt(ns);
+                                ns = '';
+                            }
+
+                            ins += a[n];
                             b = false;
                             n = -1;
                         }
                         break;
                     default:
-                        ns += c;
+                        ins += c;
                         break;
                 }
             }
-        } return ns;
+
+        } return ins;
     }
 })();
